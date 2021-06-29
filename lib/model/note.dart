f5eb4eefd3e3a522c3b59f47b54deef6493de4cf
Rafi Akmal Rahmacandran <79255405+rafi712@users.noreply.github.com>
2021-06-29 11:29:26 +0700

    Update note.dart

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -1,18 +1,6 @@
-/// Class ini merupakan model dari semua catatan
-/// Contoh constructor: 
-/// ```
-/// Note(
-///   id: 1, 
-///   title: (title == '') ? null: 'judul catatan', 
-///   description: 'deskripsi catatan', 
-///   createdAt: DateTime.now(),
-/// )
-/// ```
 class Note {
-  /// Id akan di generate otomatis (increment) melalui note provider class
   int id;
-  /// Jika title tidak ada, maka tidak akan ditampilkan, atau dianggap null
-  String? title;
+  String title;
   String description;
   String createdAt;
 
